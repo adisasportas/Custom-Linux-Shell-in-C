b829ab252f3fb7ceda640bf0197eac21a7d00386
ADI SASPORTAS <154801275+adisasportas@users.noreply.github.com>
2026-08-15 20:26:20 +0300

    Add include guards to myShell.h

--- myShell.h
+++ myShell.h
@@ -1,9 +1,12 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+void welcome(void);
 
-void welcome();
+#endif
